7a2d2410b941945d079b24f4df989ea705476130
mehrnazh <49832139+mehrnazh@users.noreply.github.com>
2026-08-01 16:28:47 +0000

    Deploying to gh-pages from @ mehrnazh/mehrnazh.github.io@44bea775c0459ea95971375c4a73c50b6768f5c9 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "george-gca/multi-language-al-folio",
+    "data-repo": "mehrnazh/mehrnazh.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
