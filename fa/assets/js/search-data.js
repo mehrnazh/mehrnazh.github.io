9e103cced146aa22257433c4dbc8eb0468733fb4
mehrnazh <49832139+mehrnazh@users.noreply.github.com>
2026-08-03 13:23:59 +0000

    Deploying to gh-pages from @ mehrnazh/mehrnazh.github.io@dad6de5aabf79cae2fd34ce27ecd633fa32251bb 🚀

--- fa/assets/js/search-data.js
+++ fa/assets/js/search-data.js
@@ -41,13 +41,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/fa/projects/";
           },
-        },{id: "nav-repositories",
-          title: "repositories",
-          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
-          section: "menu de navigation",
-          handler: () => {
-            window.location.href = "/fa/repositories/";
-          },
         },{id: "nav-cv",
           title: "cv",
           description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
@@ -62,42 +55,7 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/fa/teaching/";
           },
-        },{id: "nav-people",
-          title: "people",
-          description: "members of the lab or group",
-          section: "menu de navigation",
-          handler: () => {
-            window.location.href = "/fa/people/";
-          },
-        },{id: "dropdown-bookshelf",
-              title: "bookshelf",
-              description: "",
-              section: "déroulant",
-              handler: () => {
-                window.location.href = "/fa/books/";
-              },
-            },{id: "dropdown-blog",
-              title: "blog",
-              description: "",
-              section: "déroulant",
-              handler: () => {
-                window.location.href = "/fa/blog/";
-              },
-            },{id: "dropdown-estante-de-livros",
-              title: "estante de livros",
-              description: "",
-              section: "déroulant",
-              handler: () => {
-                window.location.href = "/fa/books/";
-              },
-            },{id: "dropdown-blog",
-              title: "blog",
-              description: "",
-              section: "déroulant",
-              handler: () => {
-                window.location.href = "/fa/blog/";
-              },
-            },{id: "post-a-post-with-plotly-js",
+        },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
         
